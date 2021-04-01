c9d924dffba3a20ec7f3ee98960903975bd51ec0
Manasmalhotra <42417319+Manasmalhotra@users.noreply.github.com>
2021-04-01 22:16:06 +0530

    Update index.js

--- GraphQL/Resolvers/index.js
+++ GraphQL/Resolvers/index.js
@@ -3,5 +3,9 @@ const userResolvers= require('./user');
 module.exports={
 Query: {
 ...postResolvers.Query
-}
-}
+},
+Mutation: {
+    ...userResolvers.Mutation,
+    ...postResolvers.Mutation
+  }
+};
